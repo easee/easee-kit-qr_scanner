18c4bef0a8099dc8d55b566da0c261f6a16a5166
Martin Pyk Pettersen <martinpyk.pettersen@easee.com>
2026-08-17 12:52:18 +0200

    chore: upgrade SDK constraints, update lifecycle event handler, and configure analyzer exclusions

--- lib/src/lifecycle_event_handler.dart
+++ lib/src/lifecycle_event_handler.dart
@@ -2,21 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 
 class LifecycleEventHandler extends WidgetsBindingObserver {
-  LifecycleEventHandler({
-    required this.resumeCallBack,
-  });
+  LifecycleEventHandler({required this.resumeCallBack});
 
   late final AsyncCallback resumeCallBack;
 
   @override
   Future<void> didChangeAppLifecycleState(AppLifecycleState state) async {
     switch (state) {
-      case AppLifecycleState.resumed:
+      case .resumed:
         await resumeCallBack();
         break;
-      case AppLifecycleState.inactive:
-      case AppLifecycleState.paused:
-      case AppLifecycleState.detached:
+      case .inactive:
+      case .paused:
+      case .detached:
+      case .hidden:
     }
   }
 }
